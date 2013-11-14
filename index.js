33c3c82ecf9aab1b4948fdb232e8fbb559e39501
eileen <826757760@qq.com>
2013-11-14 11:06:47 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,89 @@
+var fs = require('fs');
+var http = require('http');
+var server = http.createServer();
+var express = require('express');
+var app = express(server);
+var os = require('os');
+var cp = require('child_process');
+var port = 8088;
+app.get('/',function(req,res){
+    var headers = {};
+    headers["Access-Control-Allow-Origin"] = "*";
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+    headers["Access-Control-Allow-Credentials"] = true;
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authorization, Accept";
+    fs.readFile(__dirname + '/web/index.html',function(err,data){
+        if(err) throw err;
+        res.writeHead(200, headers);
+        res.write(data);
+        res.end();
+    });
+});
+app.get('/minJs',function(req,res){
+    cp.exec('node web/r.js -o web/profile.js',{},function(){// optimize=none
+        console.log(arguments);
+        res.write('minJs ok!');
+        res.end();
+    });
+
+});
+app.get('/tpls',function(req,res){
+    var out = getTpls();
+    res.write(JSON.stringify(out));
+    res.end();
+});
+app.get('/packTpls',function(req,res){
+    var out = 'jsonpCallback('+JSON.stringify(getTpls())+')';
+    fs.writeFile(__dirname+'/web/resources/tpls/tpls_pro.json',out,function(err){
+        if(err) throw err;
+        res.write('Pack partials ok!');
+        res.end();
+    });
+});
+app.get('/test',function(req,res){
+    res.write(JSON.stringify({}));
+    res.end();
+});
+function getTpls(){
+    var s = fs.readFileSync(__dirname+'/web/resources/tpls/tpls.json');
+    var arr =JSON.parse(s),l = arr.length,out = {};
+    for(var i = 0 , ln = arr.length;i<ln ; i++){
+        out[arr[i].name] = fs.readFileSync(__dirname+'/web'+arr[i].path).toString();
+    }
+    return out;
+}
+app.configure(function(){
+    app.use(express.static(__dirname + '/web'));
+});
+
+showIP();
+app.listen(port);
+console.log('listen on port : '+ port);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function showIP(){
+    var ifaces=os.networkInterfaces();
+    for (var dev in ifaces) {
+        var alias=0;
+        ifaces[dev].forEach(function(details){
+            if (details.family=='IPv4') {
+                console.log(dev+(alias?':'+alias:''),details.address);
+                ++alias;
+            }
+        });
+    }
+}
